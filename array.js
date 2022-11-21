26a7d48cfae5412a0d607a12e072e5c25db3aa84
Archana217 <archana21nimonkar@gmail.com>
2022-11-21 22:21:43 +0530

    hi

--- array.js
+++ array.js
@@ -2,3 +2,16 @@ let num=[12,13,14,15]
 console.log(num)
 //git config --global user.name "Archana217"
 //git config --global  user.email  "archana21nimonkar@gmail.com"
+
+//   git download
+// github account
+//create folder on desktop
+//git init
+//git config --global user.name "Archana217"
+//git config --global  user.email  "archana21nimonkar@gmail.com"
+//create new repo
+// git add ./  git add string.js
+// git commit -m "newprogram"
+//git branch  -M main
+//git remote add origin https://github.com/Archana217/javascript.git 
+//git push -u https://github.com/Archana217/javascriptnew.git main 
